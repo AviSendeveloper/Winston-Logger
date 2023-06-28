4cd0ff75ff9367d18c39f40f906986fed880119e
Avijit Sen <avijit.sen@tier5.in>
2023-06-28 14:00:27 +0530

    able to log in console and as well as in separate files in level base like 'info', 'warn', 'error'

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 const express = require("express");
+const expressWinston = require("express-winston");
+// const { transports } = require("express-winston");
+const { transports, format } = require("winston");
 
 const app = express();
 
+// winston
+app.use(
+    expressWinston.logger({
+        transports: [
+            new transports.Console(),
+            new transports.File({
+                // all/info
+                filename: "log/all.log",
+            }),
+            new transports.File({
+                level: 'warn',
+                filename: "log/warning.log",
+            }),
+            new transports.File({
+                level: 'error',
+                filename: "log/error.log",
+            }),
+        ],
+        format: format.combine(
+            format.json(),
+            format.timestamp(),
+            format.prettyPrint()
+        ),
+        statusLevels: true
+    })
+);
+
+// Routes
 app.get("/", (req, res, next) => {
     res.status(200).send();
 });
@@ -12,6 +43,10 @@ app.get("/200", (req, res, next) => {
 
 app.get("/400", (req, res, next) => {
     res.status(400).send();
+});
+
+app.get("/403", (req, res, next) => {
+    res.status(403).send();
 });
 
 app.get("/500", (req, res, next) => {
