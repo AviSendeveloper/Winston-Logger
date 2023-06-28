a240a4ae3c2559cdf3278f9a196edc6c2ec96f33
Avijit Sen <avijit.sen@tier5.in>
2023-06-28 16:41:55 +0530

    add DailyRotateFile from winston-daily-rotate-file package for handling auto delete log file

--- logger.js
+++ logger.js
@@ -1,7 +1,15 @@
 const { createLogger, transports, format } = require("winston");
+require("winston-daily-rotate-file");
+
+const fileRotateTransport = new transports.DailyRotateFile({
+    filename: "logs/rotate-%DATE%.log",
+    datePattern: "YYYY-MM-DD",
+    maxFiles: "14d",
+});
 
 const winstonLogger = createLogger({
     transports: [
+        fileRotateTransport,
         new transports.Console(),
         new transports.File({
             // all/info
